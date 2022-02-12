179a4214f6100154ecf7f6921197c34f5e368cc5
Stanley Pan <imoment.developer@gmail.com>
2022-02-12 13:00:00 -0500

    Initial Commit

---
+++ WaterDroplet/WaterDropletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterDropletApp.swift
+//  WaterDroplet
+//
+//  Created by Stanley Pan on 2022/02/12.
+//
+
+import SwiftUI
+
+@main
+struct WaterDropletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
